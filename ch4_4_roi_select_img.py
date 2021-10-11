1ae7b5db3631d0cb04c690cf9b5a421749c9601b
Yulia Yoon <88180839+yjioni@users.noreply.github.com>
2021-10-11 21:06:24 +0900

    Update ch4_4_roi_select_img.py

--- ch4_4_roi_select_img.py
+++ ch4_4_roi_select_img.py
@@ -8,7 +8,7 @@ if w and h:
     roi = img[y:y+h, x:x+w]
     cv2.imshow('cropped', roi)
     cv2.moveWindow('cropped', 0,0)
-    cv2.imwrite('C:/Users/oing9/Documents/Crawling/cropped.jpg', roi)
+    cv2.imwrite('./cropped.jpg', roi)
 cv2.imshow('img', img)
 cv2.waitKey()
 cv2.destroyAllWindows()
