bd30d79484e65ff675d8fd98ff31c0ae753fab00
yjioni <oing922@gmail.com>
2021-10-11 21:07:43 +0900

    selectROI

--- ch4_4_roi_select_img.py
+++ ch4_4_roi_select_img.py
@@ -1,14 +1,14 @@
 import cv2
 import numpy as np
 
-img = cv2.imread('C:/Users/oing9/Documents/Crawling/img/sunset.jpg')
+img = cv2.imread('./img/sunset.jpg')
 
 x, y, w, h = cv2.selectROI('img', img, False)
 if w and h:
     roi = img[y:y+h, x:x+w]
     cv2.imshow('cropped', roi)
     cv2.moveWindow('cropped', 0,0)
-    cv2.imwrite('C:/Users/oing9/Documents/Crawling/cropped.jpg', roi)
+    cv2.imwrite('./cropped.jpg', roi)
 cv2.imshow('img', img)
 cv2.waitKey()
 cv2.destroyAllWindows()
